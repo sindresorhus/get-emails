f3e506a8b28f1e44b23afcbddb6e27599b064571
Sindre Sorhus <sindresorhus@gmail.com>
2026-02-02 14:57:37 +0700

    Require Node.js 20

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import emailRegex from 'email-regex';
 
-const isEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const isEmailRegex = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?(?:\.[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?)*$/;
 
 export default function getEmails(string) {
 	const emails = string.match(emailRegex());
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,4 @@
 import {expectType} from 'tsd';
 import getEmails from './index.js';
 
-expectType<Set<string>>(
-	getEmails(
-		'Lorem ipsum dolor, sindresorhus@gmail.com consectetuer unicorn@rainbow.cake elit.',
-	),
-);
+expectType<Set<string>>(getEmails('Lorem ipsum dolor, sindresorhus@gmail.com consectetuer unicorn@rainbow.cake elit.'));
